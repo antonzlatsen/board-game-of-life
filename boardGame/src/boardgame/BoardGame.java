b28c9805f9cdc3f5bc2b8a0af6ac4d01ba695c43
aries2580@gmail.com <aries2580@gmail.com@f2bc8345-e462-788c-3ed4-62ee98246d8e>
2012-04-18 16:29:01 +0000

    Edited main class - board now displays on screen when the program is run. TODO - Set properties for known board pieces
    Look into - creating a Java property file for board text

--- boardGame/src/boardgame/BoardGame.java
+++ boardGame/src/boardgame/BoardGame.java
@@ -13,7 +13,11 @@ public class BoardGame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new board().setVisible(true);
+            }
+        });
+        }
 }
