e88a05471fe4b9ce5453e562a925bb1db0c2fa4c
aries2580@gmail.com <aries2580@gmail.com@f2bc8345-e462-788c-3ed4-62ee98246d8e>
2012-04-18 15:04:15 +0000

    First upload of the project file
    Task - setting tile names and text

---
+++ boardGame/src/boardgame/BoardGame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author Don
+ */
+public class BoardGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
