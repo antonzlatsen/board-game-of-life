55fc06e156261064250742e570ab2fd7ffae22da
thedardnog@gmail.com <thedardnog@gmail.com@f2bc8345-e462-788c-3ed4-62ee98246d8e>
2012-04-21 12:14:15 +0000

    Player details displayed in event log window via button click,  still need to fix it all just getting basic workings sorted

--- trunk/boardGame/src/boardgame/EventLog.java
+++ trunk/boardGame/src/boardgame/EventLog.java
@@ -7,10 +7,14 @@ package boardgame;
 import javax.swing.*;
 /**
  *
+ * 
  * @author Darren
  */
+
+
 public class EventLog extends javax.swing.JFrame {
 
+    
     /**
      * Creates new form EventLog
      */
@@ -30,13 +34,14 @@ public class EventLog extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea2 = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        PlayerDetailsText = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAlwaysOnTop(true);
         setBounds(new java.awt.Rectangle(1300, 0, 300, 0));
-        setMaximumSize(new java.awt.Dimension(400, 360));
         setMinimumSize(new java.awt.Dimension(400, 360));
-        setPreferredSize(new java.awt.Dimension(400, 360));
         setResizable(false);
 
         jTextArea2.setColumns(20);
@@ -46,6 +51,17 @@ public class EventLog extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setText("Event Log");
 
+        PlayerDetailsText.setColumns(20);
+        PlayerDetailsText.setRows(5);
+        jScrollPane2.setViewportView(PlayerDetailsText);
+
+        jButton1.setText("jButton1");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -53,26 +69,60 @@ public class EventLog extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(158, 158, 158)
+                        .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(167, 167, 167)
-                        .addComponent(jLabel1)))
-                .addContainerGap(30, Short.MAX_VALUE))
+                        .addGap(31, 31, 31)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(35, 35, 35)
+                                .addComponent(jButton1))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 438, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(8, 8, 8)
+                .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addContainerGap(51, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        // TODO add your handling code here:
+          BoardGame.missionControl.pName1=startScreen.myBoard.getjLabel1().getText();
+        BoardGame.missionControl.pName2=startScreen.myBoard.getjLabel2().getText();
+         BoardGame.missionControl.pName3=startScreen.myBoard.getjLabel3().getText();
+         BoardGame.missionControl.pName4=startScreen.myBoard.getjLabel4().getText();
+          BoardGame.missionControl.pName5=startScreen.myBoard.getjLabel5().getText();
+         BoardGame.missionControl.pName6=startScreen.myBoard.getjLabel6().getText();
+         
+         
+       BoardGame.missionControl.pNames[0]=BoardGame.missionControl.pName1;
+       BoardGame.missionControl.pNames[1]=BoardGame.missionControl.pName2;
+       BoardGame.missionControl.pNames[2]=BoardGame.missionControl.pName3;
+       BoardGame.missionControl.pNames[3]=BoardGame.missionControl.pName4;
+       BoardGame.missionControl.pNames[4]=BoardGame.missionControl.pName5;
+       BoardGame.missionControl.pNames[5]=BoardGame.missionControl.pName6;
+       
+       // int currentPlayer=BoardGame.missionControl.playerTurn;
+        
+        String PlayerDetails=Integer.toString(BoardGame.missionControl.players[0].getMoney());
+        PlayerDetailsText.setText(BoardGame.missionControl.pNames[0] +" Total money = " + PlayerDetails);
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -109,17 +159,33 @@ public class EventLog extends javax.swing.JFrame {
          */
         java.awt.EventQueue.invokeLater(new Runnable() {
 
+            
+            
             public void run() {
                 new EventLog().setVisible(true);
+             
+             
+     
             }
         });
     }
           public JTextArea getjTextArea2() {
         return jTextArea2;
-    }
+          }   
+     
+         
+     
+          
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea PlayerDetailsText;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea jTextArea2;
     // End of variables declaration//GEN-END:variables
+ 
+    
+   
+   
 }
