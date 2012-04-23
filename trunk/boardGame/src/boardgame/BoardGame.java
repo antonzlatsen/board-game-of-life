5d0d0b97d1a6ef640003eea108579b01964dfaf7
paddym1991@googlemail.com <paddym1991@googlemail.com@f2bc8345-e462-788c-3ed4-62ee98246d8e>
2012-04-23 19:55:51 +0000

    1. got rid of islandSelectGUI.java and replaced it with a method in Houston which spawns a JOption pane 2. cleaned movePlayer method got rid of a lot of useless code 3. getUserIsland method is Houston 4. added validation for erupt button and end turn button

--- trunk/boardGame/src/boardgame/BoardGame.java
+++ trunk/boardGame/src/boardgame/BoardGame.java
@@ -51,11 +51,11 @@ public static Houston missionControl = new Houston();
 
 /**     
  *      TODO    0.  Start populating this area with things that need done
- *      TODO    1.  new form for path switching 
-        TODO    2.  Validate the eruption button and the end turn button
-        TODO    3.  make islands within island (islandception)
-        TODO  3.1.  warning Islands have different paths - but movement is now broken (or working as expected depending on how you look at it).
-        TODO    3.2 make messagebox to allow for path switching
+ *      TODO    ******1.  new form for path switching - done 
+        TODO    ******2.  Validate the eruption button and the end turn button -done
+        TODO    ******3.  make islands within island (islandception) -done
+        TODO    ******3.1.  warning Islands have different paths - but movement is now broken (or working as expected depending on how you look at it). -done
+        TODO    ******3.2 make messagebox to allow for path switching - done 
         TODO    4.  Start naming controls as they are added - this will save a lot of time with renaming and creaking code later on
         TODO    5. validation needs put into buttons, player can end turn without choosing an island when they land on a yellow tile
  */
