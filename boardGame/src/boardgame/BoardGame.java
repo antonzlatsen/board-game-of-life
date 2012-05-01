ce838b884b1546fd4939edf288d06c16ec29f2b1
aries2580@gmail.com <aries2580@gmail.com@f2bc8345-e462-788c-3ed4-62ee98246d8e>
2012-05-01 21:22:54 +0000

    deleted todos and random comments

--- boardGame/src/boardgame/BoardGame.java
+++ boardGame/src/boardgame/BoardGame.java
@@ -23,20 +23,7 @@ public static tiles boardTiles = new tiles();
     
         public static void main(String args[]) throws FileNotFoundException, IOException{
         
-                //testing code     
-                //boardgame.startScreen.myBoard.setVisible(true);
-                //array[5].add(startScreen.myBoard.getLblPlayer1());
-            
-
-            /**
-             *      WASTED CODE - LOOK INTO LATER - FOR NOW THIS WILL SHOW THE 
-             *      START SCREEN - THE BUTTON HAS ALL THE CODE NEEDED TO SET UP THE GAME
-             * 
-             *
-             *      // java.awt.EventQueue.invokeLater(new Runnable() {
-             *      //public void run() {}});
-             */
-            
+          
                 //  Shows the Start screen - used for selecting players
                 //  and adding names
                 new startScreen().setVisible(true);
@@ -66,12 +53,8 @@ public static tiles boardTiles = new tiles();
                    
                     AudioPlayer.player.start(musicStream);
 
-                        
-
         }
 
-        
-        
 
 }
 
@@ -80,34 +63,4 @@ public static tiles boardTiles = new tiles();
 //      This will now be the place to add to-do's 
 //      When you click on tasks - you can see what there is still left to do
 //      Hopefully will keep us up to date on things instead of having to look at the logs on SVN
-
-
-/**     
-        TODO    - Lots of testing and adding more tiles
-        TODO    - Randomizing tiles needs tweaked so that popups are not showing up everywhere, e.g all tiles become a port (travel tile)
-        TODO    - Clean up the board.java code - very messy in there
-        TODO    - More outputs to tell us what is happening with each player
-        TODO    - Code for the exam red tile on the middle island
-        TODO    - Add code to find what tile the player has landed on
-        TODO    - Scoring for when game is finished
-        TODO    - StartScreen needs a label to inform the player what the check boxes are for
-        TODO    - College pay add a green tile that is pay college fees
-        TODO    - Add spin to win functionality
-
-        TODO    - Lots of testing and adding more tiles 
-        TODO    - Randomizing tiles needs tweaked so that pop ups are not showing up everywhere, e.g all tiles become a port (travel tile) 
-        TODO    - Clean up the board.java code - very messy in there 
-        TODO    - More outputs to tell us what is happening with each player 
-        TODO    - Code for the exam red tile on the middle island
-        TODO    - Add code to find what tile the player has landed on
-        TODO    - Progress bar decreases when a 10 is rolled
-        TODO    - Scoring for when game is finished
-        TODO    - Player icons need slight formatting
-        TODO    - College pay add a green tile that is pay college fees
-        TODO    - Add spin to win functionality
-        TODO    - add pet tile 
-        TODO    - Rename class files with capital letters for first character
-        TODO    - START screen text box char limit
-        TODO    - exams volcano cant go off when uyou are doing exams
-        
- */
+//      TODOS ARE DONE! :)
